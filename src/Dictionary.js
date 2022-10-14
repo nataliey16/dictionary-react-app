4cb5678d5ff56fb789280d7398ee51c186e6b9e6
nataliey16 <102062208+nataliey16@users.noreply.github.com>
2022-10-14 10:42:48 -0600

    Adjusted the css on button and adding js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,54 +6,66 @@ import "./Dictionary.css";
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState("");
+  let [loaded, setLoaded] = useState(false);
 
   function handleSearchResponse(response) {
-    console.log(response.data[0]);
     setResults(response.data[0]);
+  }
+
+  function search() {
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleSearchResponse);
   }
 
   function handleSearchSubmit(event) {
     event.preventDefault();
-
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleSearchResponse);
   }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
 
-  return (
-    <div className="Dictionary">
-      <section>
-        <form
-          className="Dictionary-search-engine"
-          onChange={handleKeywordChange}
-        >
-          <div className="row">
-            <div className="col-lg-8 col-lg-8">
-              <input
-                className="form-control search-bar"
-                type="search"
-                placeholder="Enter a word"
-                autoFocus="on"
-              ></input>
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <form
+            className="Dictionary-search-engine"
+            onChange={handleKeywordChange}
+          >
+            <div className="row">
+              <div className="col-lg-8 col-lg-8">
+                <input
+                  className="form-control search-bar"
+                  type="search"
+                  placeholder="Enter a word"
+                  autoFocus="on"
+                ></input>
+              </div>
+              <div className="col-lg-4 col-lg-4">
+                <input
+                  className="btn btn-branding"
+                  type="submit"
+                  value="Search"
+                  onClick={handleSearchSubmit}
+                ></input>
+              </div>
             </div>
-            <div className="col-lg-4 col-lg-4">
-              <input
-                className="btn btn-branding"
-                type="submit"
-                value="Search"
-                onClick={handleSearchSubmit}
-              ></input>
-            </div>
+          </form>
+          <div className="suggested-words">
+            suggested keywords: sunset, education, composting ...
           </div>
-        </form>
-        <div className="suggested-words">
-          suggested keywords: sunset, education, composting ...
-        </div>
-      </section>
-      <Results results={results} />
-    </div>
-  );
+        </section>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    load();
+    return <div>Loading...</div>;
+  }
 }
